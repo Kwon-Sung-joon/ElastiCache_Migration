7291001232ea388a7817ab5b33899714c99a2125
SungJoon Kwon <43159901+Kwon-Sung-joon@users.noreply.github.com>
2023-11-29 09:58:46 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,9 +4,9 @@ import logging
 from multiprocessing import Process
 
 #src = redis.cluster.RedisCluster(host='dev-p-redis.uaz5x4.clustercfg.apn2.cache.amazonaws.com',port=6379,decode_responses=True)
-
 dst = redis.cluster.RedisCluster(host='tmp-redis.uaz5x4.clustercfg.apn2.cache.amazonaws.com',port=6379,decode_responses=True)
-idc=redis.Redis(host='172.16.0.60',port=6379,decode_responses=True,password='Tide!fpeltm9')
+
+idc=redis.Redis(host='172.16.0.60',port=6379,decode_responses=True,password='test1234')
 
 
 
@@ -26,35 +26,34 @@ def migration(keys):
         dst.restore(str(key),ttl,value,replace=True)
 
 def lambda_handler(event, context):
- 
     #data migrations cluster to cluster
     cur = 0
-    #cur 63662
-    #cur = 120727
     key_cnt=0
     flag=0;
     
-    # create a list to keep all processes
-    processes = []
+    #using multiprocess
+    #processes = []
     
     while True :
-        #cluster scan
+        
+        #aws cluster scan
         #scans=src.scan(cursor=cur,count=1000)
         #keys=scans[-1]
         #cur=list(scans[0].values())[0]
         
-        #idc scan
+        #idc redis scan
         scans=idc.scan(cursor=cur,count=1000)
         keys=scans[-1]
         cur=scans[0]
         migration(keys)
         
         
-        #set processes ...
+        #using multiprocess
         #process = Process(target=migration,args=([keys]));
-        #processes.append(process)
+        
+        
         #data mig
-        #migration(keys)
+        migration(keys)
         flag+=1;
         key_cnt+=len(keys)
         print("last cursor is {0}".format(cur))
